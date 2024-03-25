647937ec64300da418d5953e7bba960e6a7e5213
scenaristeur <scenaristeur@gmail.com>
2024-03-25 13:29:27 +0100

    form

--- src/views/CreateThing.vue
+++ src/views/CreateThing.vue
@@ -4,7 +4,7 @@
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">name</span>
             <input type="text" class="form-control" v-model="thing.name" placeholder="ex: Mon idée" aria-label="thing"
-                aria-describedby="basic-addon1">
+                aria-describedby="basic-addon1" @keyup.enter="create()">
         </div>
         <button v-on:click="create()" type="button" class="btn btn-success">create</button>
     </div>
@@ -13,17 +13,30 @@
 <script>
 export default {
     name: "CreateThing",
-    data(){
+    data() {
         return {
-            thing: {
+            thing_default: {
                 name: ""
-            }
+            },
+            thing: {}
         }
     },
-    methods:{
-        create(){
+    created() {
+        this.reset()
+    },
+    methods: {
+        reset() {
+            this.thing = Object.assign({}, this.thing_default)
+        },
+        create() {
             console.log("CreateThing", this.thing)
-            this.$store.dispatch("fluree/createThing", this.thing)
+            if (this.thing.name.length < 1) {
+                alert("name must be at least 1 character long")
+            } else {
+                this.$store.dispatch("fluree/createThing", this.thing)
+                this.reset()
+            }
+
         }
     }
 
